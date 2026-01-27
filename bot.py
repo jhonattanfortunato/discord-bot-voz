a7e06b387464e16b189c1c1f32b32c4ea0254c48
Jhonattan <jhonattan.mirim@gmail.com>
2026-01-27 20:36:38 -0300

    Ativa message content intent

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ intents = discord.Intents.default()
 intents.voice_states = True
 intents.guilds = True
 intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
